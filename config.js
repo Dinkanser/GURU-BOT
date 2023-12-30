36b6358d6403d92411c612e6c38982dcb9f61535
Dinkanser <155206241+Dinkanser@users.noreply.github.com>
2023-12-30 09:31:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['919744108970', 'dinkanser', true],
+  ['919744108970', 'dinkensr', true], 
   [''] 
 ] //Number of owners
 
@@ -50,16 +50,16 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
-global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
-global.menuvid = 'https://i.imgur.com/AjDzGgG.mp4'
-global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
-global.dygp = 'https://chat.whatsapp.com/BFfD1C0mTDDDfVdKPkxRAA'
-global.fgsc = 'https://github.com/Guru322/GURU-BOT' 
-global.fgyt = 'https://youtube.com/@Asliguru'
-global.fgpyp = 'https://youtube.com/@Asliguru'
+global.botname = 'dinkenmd'
+global.premium = 'false'
+global.packname = 'dinkanmd' 
+global.author = '@dinkanser' 
+global.menuvid = 'https://imgur.com/a/JbIljlZ'
+global.igfg = '▢ Follow on Instagram\https://www.instagram.com/dinkan_ser?igsh=MzNlNGNkZWQ4Mg==' 
+global.dygp = 'https://chat.whatsapp.com/Hvodr8aHAkI8SCEuChAg1Q'
+global.fgsc = 'https://github.com/Dinkanser/GURU-BOT' 
+global.fgyt = 'https://youtube.com/@null'
+global.fgpyp = 'https://youtube.com/@null'
 global.fglog = 'https://raw.githubusercontent.com/Guru322/api/Guru/guru.jpg' 
 global.thumb = fs.readFileSync('./Assets/Gurulogo.jpg')
 
